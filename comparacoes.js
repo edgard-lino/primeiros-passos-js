df0789130e6d4cf044d0c68b055a76a1c3134574
Edgard Lino da Silva <mosmack@gmail.com>
2022-12-04 17:21:25 -0300

    comparacoes

---
+++ comparacoes.js
@@ -0,0 +1,15 @@
+// == comparação implícita
+const numero = 5;
+const texto = "5";
+console.log(numero == texto);
+console.log(numero === texto);
+
+// typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara o valor
+// === compara valor e o tipo de dado
+// conversão explícita
+Number();
+String();
---
+++ script.js
@@ -0,0 +1,14 @@
+// tipagem dinâmica
+// untyped
+let minhaVar = 567;
+minhaVar = "texto";
+
+// O JS é MULTIPARADIGMA (Consegue resolver o problema de várias formas)
+
+// NODE - interpretador do JS no BackEnd.
+// é o ambiente para gerar JS fora do navegador.
+
+console.error("deu erro");
+
+let number = 1;
+console.time();
